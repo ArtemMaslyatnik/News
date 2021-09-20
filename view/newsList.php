d92ee8b0a32926de573c9436f0300ee413f0c1a6
Artem <maslytnik@gmail.com>
2021-09-20 11:27:59 +0300

    implement dynamic post output

--- view/newsList.php
+++ view/newsList.php
@@ -1,27 +1,18 @@
 <!-- list -->
+
 <div class="col-md-12 text-center">
     <h3 class="pb-4 mb-4 fst-italic border-bottom">
         Новости
     </h3>
-
+   
     <article class="blog-post">
-        <h2 class="blog-post-title">Простой блог</h2>
-        <p class="blog-post-meta">Январь 1, 2021 автор <a href="#">Иван</a></p>
+        <?php 
+         $arrNews = include("./components/News.php");
+        foreach ($arrNews as $newsList):?>
+        <h2 class="blog-post-title"><?php echo $newsList['title'];?></h2>
+        <p class="blog-post-meta"><?php echo $newsList['content'];?></p>
+        <p class="blog-post-meta"><?php echo $newsList['date'];?><a href="#"><?php echo $newsList['author_name'];?></a></p>
+        <?php endforeach;?>
     </article>
 
-    <article class="blog-post">
-        <h2 class="blog-post-title">Еще новость</h2>
-        <p class="blog-post-meta">Май 23, 2020 автор <a href="#">Яков</a></p>
-    </article>
-
-    <article class="blog-post">
-        <h2 class="blog-post-title">Новая особенность</h2>
-        <p class="blog-post-meta">Май 14, 2020 автор <a href="#">Марк</a></p>
-
-    </article>
-
-    <nav class="blog-pagination" aria-label="Pagination">
-        <a class="btn btn-outline-primary" href="#">Старые</a>
-        <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Новые</a>
-    </nav>
 </div>
